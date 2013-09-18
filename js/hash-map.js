8d91ddda889172d2eb2a32d56bf65b4e3687e1eb
Robert Ödling <robert.odling@gmail.com>
2013-09-18 12:55:50 +0200

    Initial version of hashmap

--- js/hash-map.js
+++ js/hash-map.js
@@ -11,23 +11,118 @@ var HashMap = (function () {
 
 	'use strict';
 
-	var HashMap = function (){
+	/**
+	 * Initialize map
+	 * @constructor
+	 */
+	var HashMap = function () {
 		this.map = {};
 	};
 
 	var proto = HashMap.prototype;
 
-	proto.size = function (){
+	/**
+	 * Returns size, the number of entries (or more correct number of keys) int the map.
+	 * @return {number}
+	 */
+	proto.size = function () {
 		var size = 0;
-		for(var key in this.map){
+
+		this.forEach(function () {
 			size++;
-		}
+		});
+
 		return size;
 	};
 
-	proto.put = function (key, value){
+	/**
+	 * Checks if specified value is undefined or null, used because we want to allow falsy strings, numbers and booleans
+	 * @param {*}
+	 * @return {boolean}
+	 * @private
+	 */
+	function _isSomeNullOrUndefined() {
+		var args = [].slice.call(arguments);
+
+		return args.some(function (value) {
+			// NOTE: '==' instead of '===' as we want to include undefined also
+			return value == null;
+		});
+	}
+
+	proto.put = function (key, value) {
+
+		if (_isSomeNullOrUndefined(key, value)) {
+			return;
+		}
+
 		this.map[key] = value;
 	};
+
+	proto.get = function (key) {
+
+		if (_isSomeNullOrUndefined(key)) {
+			return;
+		}
+
+		// do not return values from prototype chain
+		if (this.map.hasOwnProperty(key)) {
+			return this.map[key];
+		}
+	};
+
+	proto.remove = function (key) {
+
+		if (_isSomeNullOrUndefined(key)) {
+			return;
+		}
+
+		// do not allow removing from prototype chain
+		if (this.map.hasOwnProperty(key)) {
+			delete this.map[key];
+		}
+	};
+
+	proto.values = function () {
+		var values = [];
+		this.forEach(function (key, value) {
+			values.push(value);
+		});
+		return values;
+	};
+
+	proto.keys = function () {
+		var keys = [];
+
+		this.forEach(function (key) {
+			keys.push(key);
+		});
+
+		return keys;
+	};
+
+	proto.isEmpty = function () {
+		return this.size() === 0;
+	};
+
+	proto.clear = function () {
+
+		// NOTE: It would have been enough with "this.map = {}"
+		// but this approach should hopefully speed up garbage collecting
+		this.forEach(function (key, value) {
+			delete (this.map[key]);
+		}.bind(this));
+	};
+
+	proto.forEach = function (cb) {
+
+		for (var key in this.map) {
+			if (this.map.hasOwnProperty(key)) {
+				cb(key, this.map[key]);
+			}
+		}
+	};
+
 
 	// export HashMap constructor
 	return HashMap;
--- specs/hash-map.spec.js
+++ specs/hash-map.spec.js
@@ -1,5 +1,7 @@
+/* global describe, it, expect, HashMap*/
 describe('HashMap', function () {
 
+	'use strict';
 
 	function batcher(map, count, fn) {
 		for (var i = 0; i < count; i++) {
@@ -8,8 +10,8 @@ describe('HashMap', function () {
 	}
 
 
-	describe('size', function () {
-		it('should have size 0 if empty', function () {
+	describe('Put and size', function () {
+		it('should have size 0 if no item added', function () {
 			var map = new HashMap();
 			expect(map.size()).to.equal(0);
 		});
@@ -27,6 +29,172 @@ describe('HashMap', function () {
 			});
 			expect(map.size()).to.equal(1024);
 		});
+
+		it('should not increase size if putting with same key', function () {
+			var map = new HashMap();
+			map.put('one', 1);
+			map.put('one', 2);
+			expect(map.size()).to.equal(1);
+		});
+
+		it('should not increase size if putting null', function () {
+			var map = new HashMap();
+			map.put('one', null);
+			expect(map.size()).to.equal(0);
+		});
+
+
+		it('should not increase size if putting undefined', function () {
+			var map = new HashMap();
+			map.put('one', undefined);
+			expect(map.size()).to.equal(0);
+		});
+
+
 	});
+
+	describe('Getting from map', function () {
+
+		it('should return added item', function () {
+			var map = new HashMap();
+			map.put('foo', 'bar');
+			expect(map.get('foo')).to.equal('bar');
+		});
+
+		it('should return "falsy" string', function () {
+			var map = new HashMap();
+			map.put('falsy', '');
+			expect(map.get('falsy')).to.equal('');
+			expect(map.get('falsy')).to.be.an('string');
+		});
+
+		it('should return "falsy" number', function () {
+			var map = new HashMap();
+			map.put('falsy', 0);
+			expect(map.get('falsy')).to.equal(0);
+			expect(map.get('falsy')).to.be.an('number');
+		});
+
+		it('should return "falsy" boolean', function () {
+			var map = new HashMap();
+			map.put('falsy', false);
+			expect(map.get('falsy')).to.equal(false);
+			expect(map.get('falsy')).to.be.an('boolean');
+		});
+
+		it('should return last value added for key', function () {
+			var map = new HashMap();
+			map.put('foo', 'not_bar');
+			map.put('foo', 'bar');
+			expect(map.get('foo')).to.equal('bar');
+		});
+
+	});
+
+	describe('Removing from map', function () {
+
+		it('should correctly remove item', function () {
+			var map = new HashMap();
+			map.put('foo', 'bar');
+			map.remove('foo');
+			expect(map.get('foo')).to.not.exist;
+		});
+
+	});
+
+	describe('Get values from map', function () {
+
+		it('should return values as array', function () {
+			var map = new HashMap();
+			map.put('one', 1);
+			map.put('two', 2);
+			map.put('three', 3);
+			var values = map.values();
+
+			// order is not preserved
+			expect(values.indexOf(1)).to.be.at.least(0);
+			expect(values.indexOf(2)).to.be.at.least(0);
+			expect(values.indexOf(3)).to.be.at.least(0);
+		});
+
+	});
+
+	describe('Get keys from map', function () {
+
+		it('should return keys as array', function () {
+			var map = new HashMap();
+			map.put('one', 1);
+			map.put('two', 2);
+			map.put('three', 3);
+			var keys = map.keys();
+
+			// order is not preserved
+			expect(keys.indexOf('one')).to.be.at.least(0);
+			expect(keys.indexOf('one')).to.be.at.least(0);
+			expect(keys.indexOf('one')).to.be.at.least(0);
+		});
+
+	});
+
+	describe('Testing isEmpty on map', function () {
+
+		it('shold return true on newly created map', function () {
+			var map = new HashMap();
+			expect(map.isEmpty()).to.be.true;
+		});
+
+		it('shold return true after adding and removing', function () {
+			var map = new HashMap();
+			map.put('one', 1);
+			map.put('two', 2);
+			map.put('three', 3);
+			map.remove('one');
+			map.remove('one');
+			map.remove('two');
+			map.remove('three');
+			expect(map.isEmpty()).to.be.true;
+		});
+
+		it('shold return true after adding and clearing', function () {
+			var map = new HashMap();
+			map.put('one', 1);
+			map.put('two', 2);
+			map.put('three', 3);
+			map.clear();
+			expect(map.isEmpty()).to.be.true;
+		});
+
+	});
+
+	describe('Iterating items of map', function () {
+
+
+		it('should run callback for each element', function () {
+			var map = new HashMap();
+			map.put('one', 1);
+			map.put('two', 2);
+			map.put('three', 3);
+			var keys = [];
+			var values = [];
+
+			// Order is not preserved
+			map.forEach(function (key, value) {
+
+				keys.push(key);
+				values.push(value);
+			});
+
+			expect(keys.indexOf('one')).to.be.at.least(0);
+			expect(keys.indexOf('two')).to.be.at.least(0);
+			expect(keys.indexOf('three')).to.be.at.least(0);
+			expect(values.indexOf(1)).to.be.at.least(0);
+			expect(values.indexOf(2)).to.be.at.least(0);
+			expect(values.indexOf(3)).to.be.at.least(0);
+		});
+
+
+	});
+
+
 });
 
